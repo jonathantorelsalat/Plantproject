726541b914553a655134e2085d58b37ed5de04f1
jonathantorelsalat <jonath_ts@hotmail.fr>
2018-04-16 17:23:18 +0200

    Update MS_star_index.sh

--- MS_star_index.sh
+++ MS_star_index.sh
@@ -5,4 +5,4 @@
 
 #avec faux GTF
 
-/opt/STAR/bin/Linux_x86_64/STAR --runMode genomeGenerate --runThreadN 40 --limitGenomeGenerateRAM=120000000000 --genomeDir /store/EQUIPES/PBI/Jonathan/Medicago_sativa/FASTQ/index_fakeGTF --genomeFastaFiles /store/EQUIPES/PBI/Jonathan/Medicago_sativa/FASTQ/Alfalfa_112K.fasta --sjdbGTFfile /store/EQUIPES/PBI/Jonathan/Medicago_sativa/fake_gtf/faux_GTF.gtf
+/opt/STAR/bin/Linux_x86_64/STAR --runMode genomeGenerate --runThreadN 40 --limitGenomeGenerateRAM=120000000000 --genomeDir /store/EQUIPES/PBI/Jonathan/Medicago_sativa/FASTQ/index_fakeGTF --genomeFastaFiles /store/EQUIPES/PBI/Jonathan/Medicago_sativa/FASTQ/Alfalfa_112K.fasta 
